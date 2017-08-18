279332fbed42d4358dab9531e7f8f91eb0e63824
Rob Weber <rob.weber87@gmail.com>
2017-08-18 17:03:58 -0400

    add footer to homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Footer from './Footer'
 import './App.css';
 
 class App extends Component {
@@ -33,6 +34,12 @@ class App extends Component {
         <div className="container">
           <a href="#"><img className="logo" src={`/assets/rcsmd_${this.state.pic}.png`} alt="" onClick={() => this.incrementNum()} /></a>
         </div>
+        <div className="container">
+          <h3>The purpose of the group is to denounce all recycling...ever! It's pointless and doesn't save the environment</h3>
+          <h4>For further questions click the envelope below or email the group organizer at D-Bagz@recyclingcansuckmydick.com</h4>
+          <h6>he's a real sweetheart ;)</h6>
+        </div>
+        <Footer />
       </div>
     );
   }
---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+
+	return(
+		<footer className="footer">
+			<a href="https://www.facebook.com/RecycleThisDick/" ><i className="fa fa-facebook-square fa-2x grey" aria-hidden="true"></i></a>
+			<a href="https://www.instagram.com/recycling_can_suck_my_dick/" ><i className="fa fa-instagram fa-2x grey" aria-hidden="true"></i></a>
+			<a href="https://twitter.com/RecycleThisDick" ><i className="fa fa-twitter-square fa-2x grey" aria-hidden="true"></i></a>
+			<a href="mailto:D-Bagz@recyclingcansuckmydick.com" ><i className="fa fa-envelope-open fa-2x grey" aria-hidden="true"></i></a>
+		</footer>
+	)
+
+}
+
+export default Footer
