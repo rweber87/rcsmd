1eb6836c42407d6778de23ca08ba2099e0ddd67b
Rob Weber <rob.weber87@gmail.com>
2017-08-18 15:58:36 -0400

    initial app

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      pic: 1
+    }
+
+  }
+
+  incrementNum() {
+    var pic = this.state.pic + 1
+    if(pic > 7) {
+        this.setState({
+          pic: 1
+        })  
+    } else {
+      this.setState({
+        pic: pic
+      })
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <h2>Welcome to Recycling Can Suck My Dick</h2>
+        <p>Click the image below!</p>
+        <div className="container">
+          <a href="#"><img className="logo" src={`/assets/rcsmd_${this.state.pic}.png`} alt="" onClick={() => this.incrementNum()} /></a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
